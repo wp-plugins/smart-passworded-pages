271c6553ba1572370f9fc22685dbd13f6670058a
BrianLayman <BrianLayman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-08 19:19:56 +0000

    Initial Check In
    git-svn-id: https://plugins.svn.wordpress.org/smart-passworded-pages/trunk@554974 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== Smart Passworded Pages ===
+Contributors: BrianLayman 
+Donate link:http://thecodecave.com/donate
+Tags: password,security,page,member,login,cms
+Requires at least: 2.5
+Tested up to: 3.3.2
+Stable tag: 1.0
+
+Allows a central login page for password protected child pages. Enter a password and you are taken to the newest child page with a matching password.
+
+== Description ==
+
+The Smart Passworded Pages enhances WordPress by allowing the creation of central login pages that grant access to a any number of passworded child pages. In this fashion you can give each client/member/organization a central place to enter a password and they will be taken to the page that has their information.  
+  
+The password field is displayed as a field followed by a button with customizable text. The form is can be uniquely stylized with CSS. The child pages can in turn link to other pages protected with the same password and the password will not need to be re-entered.  
+  
+To add the password field to a page, simply enter the short code  
+[smartpwpages]  
+  
+If you wish to assign a unique label to the submit button or give the form a unique ID for CSS identification, the attributes in the following example can be used:  
+[smartpwpages label=\"Login\" ID=\"sppForm1\"]
+
+== Installation ==
+
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
+== Screenshots ==
+1. A login form can be added to any page. That login form will take you to the newest child page that has a matching password.
+
+This is a screen shot of the first demo page for the plugin:
+
+<a href=\"http://thecodecave.com/smart-passworded-pages-demonstration-1/\" title=\"The first demo page for the plugin\">http://thecodecave.com/smart-passworded-pages-demonstration-1/</a>
+
+There is also the second demo page for the plugin:
+
+<a href=\"http://thecodecave.com/smart-passworded-pages-demonstration-1/\" title=\"The second demo page for the plugin\">http://thecodecave.com/smart-passworded-pages-demonstration-2/</a> 
+
+2. The plugin is activated through the simple use of a shortcode in a post.
+
+This video describes the usage of the plugin:
+ `[youtube http://www.youtube.com/watch?v=DTliSyBPBWI]`
+ 
+== Changelog ==
+1.0 Initial Release
---
+++ smartpwpages.php
@@ -0,0 +1,89 @@
+<?php
+/*
+Plugin Name: Smart Passworded Pages
+Plugin URI: http://thecodecave.com/plugins/smart-passworded-pages-plugin/
+Description: Allows a central login page for password protected child pages. Enter a password and you are taken to the newest child page with a matching password.
+Version: 1.0
+Author: Brian Layman
+Author URI: http://eHermitsInc.com/
+License: GPL2
+Requires: 2.5
+
+Copyright 2012  Brian Layman  (email : plugins@thecodecave.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as 
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+class smartPWPages {
+     function smartpwpages_shortcode( $atts ) {
+			global $post;
+			extract( shortcode_atts( array(
+				'label' => 'Enter',
+				'ID' => 'smartPWLogin',
+			), $atts ) );
+			$result =  '<form ID="' . $ID . '" method="post" action="' . get_permalink() . '" >' . PHP_EOL;
+			if ( isset( $_GET['wrongpw'] ) ) $result .= '<p id="smartPWError">You\'ve entered an invalid password.</p>' . PHP_EOL;
+			$result .= '	<input class="requiredField" type="password" name="smartPassword" id="smartPassword" value=""/>' . PHP_EOL;
+			$result .= '	<input type="hidden" name="smartParent" value="' .  $post->ID . '" />' . PHP_EOL;
+			$result .= '	<input type="hidden" name="smartPWPage_nonce" value="' . wp_create_nonce( smartPWPage ).'" />' . PHP_EOL;
+			$result .= '	<input type="submit" value="' . $label . '" />' . PHP_EOL;
+			$result .= '</form>' . PHP_EOL;
+            return $result;
+     }
+
+	function pw_redirect( $perma, $password ) {
+		if ( empty( $wp_hasher ) ) {
+			require_once( ABSPATH . 'wp-includes/class-phpass.php' );
+			// By default, use the portable hash from phpass
+			$wp_hasher = new PasswordHash(8, true);
+		}
+		// 10 days
+		setcookie( 'wp-postpass_' . COOKIEHASH, stripslashes( $password ), time() + 864000, COOKIEPATH );
+		wp_safe_redirect( $perma );
+		exit();
+	}
+	
+	function process_form() {
+		if ( isset( $_POST[ 'smartPassword' ] ) && isset( $_POST[ 'smartParent' ] ) && wp_verify_nonce( $_POST['smartPWPage_nonce'], smartPWPage ) ) {
+			$parentForm  = (int) $_POST[ 'smartParent' ] ;
+			$postPassword = stripslashes( $_POST[ 'smartPassword' ] );
+
+			$args = array(		
+				'sort_order' => 'DESC',
+				'sort_column' => 'post_date',
+				'hierarchical' => 1,
+				'child_of' => $parentForm,
+				'parent' => $parentForm,
+				'post_type' => 'page',
+				'post_status' => 'publish'
+			);
+
+			$myPages = get_pages( $args );
+
+			foreach( $myPages as $page ) {
+				if ( $page->post_password == $postPassword ) {
+					$permalink = get_permalink( $page->ID );
+					$this->pw_redirect( $permalink, $postPassword );
+				}
+			}
+			// Nothing more to do here. If we reached here, we submitted a pw but no match was found. 
+			// Allow the page to continue loading, but hack $_GET to indicate the status
+			$_GET['wrongpw'] = TRUE;
+		}
+	}
+}
+
+$smartPWPages = new smartPWPages();
+add_action('init', array($smartPWPages, 'process_form' ) );
+add_shortcode( 'smartpwpages', array($smartPWPages, 'smartpwpages_shortcode') );
