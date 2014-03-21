f6f6255aa4723ed8497ca501e26abc3331cb3ca6
BrianLayman <BrianLayman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-21 19:02:32 +0000

    Updated for the current version of WordPress
    git-svn-id: https://plugins.svn.wordpress.org/smart-passworded-pages/branches/1.1.5@879701 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,78 @@
+=== Smart Passworded Pages ===
+Contributors: BrianLayman 
+Donate link: http://thecodecave.com/donate
+Tags: password,security,page,member,login,cms
+Requires at least: 2.5
+Tested up to: 3.8.1
+Stable tag: 1.1.5
+
+Allows a central login page for password protected child pages. Enter a password and you are taken to the newest child page with a matching password.
+
+== Description ==
+
+The Smart Passworded Pages enhances WordPress by allowing the creation of central login pages that grant access to a any number of passworded child pages. In this fashion you can give each client/member/organization a central place to enter a password and they will be taken to the page that has their information.  
+  
+The password field is displayed as a field followed by a button with customizable text. The form is can be uniquely stylized with CSS. The child pages can in turn link to other pages protected with the same password and the password will not need to be re-entered.  
+  
+To add the password field to a page, simply enter the short code  
+[smartpwpages]  
+  
+If you wish to assign a unique label to the submit button or give the form a unique ID for CSS identification, the attributes in the following example can be used:  
+[smartpwpages label=\"Login\" ID=\"sppForm1\"]
+
+This plugin doesn't add the ability to add passwords to pages.  WordPress has that built in.  On the right hand side of the page editing screen in WordPress, you can change the visibility to Password protected and enter in a password. If you are unfamiliar with using passwords in WordPress, you might want to read this page first:  http://codex.wordpress.org/Using_Password_Protection
+
+This plugin does make the password handling smarter and enhances it so that you can enter one password on a parent page and gain access to all the children pages using that password.  If you don't know what children pages or sub-pages are, you might want to read about it here:  http://codex.wordpress.org/Pages#Creating_Pages
+
+You can find out more about the Smart Passworded Pages plugin here: http://thecodecave.com/smart-passworded-pages-plugin/
+
+== Installation ==
+
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
+== Screenshots ==
+1. A login form can be added to any page. That login form will take you to the newest child page that has a matching password.
+
+This is a screen shot of the first demo page for the plugin:
+
+<a href=\"http://thecodecave.com/smart-passworded-pages-demonstration-1/\" title=\"The first demo page for the plugin\">http://thecodecave.com/smart-passworded-pages-demonstration-1/</a>
+
+There is also the second demo page for the plugin:
+
+<a href=\"http://thecodecave.com/smart-passworded-pages-demonstration-1/\" title=\"The second demo page for the plugin\">http://thecodecave.com/smart-passworded-pages-demonstration-2/</a> 
+
+2. The plugin is activated through the simple use of a shortcode in a post.
+
+This video describes the usage of the plugin:
+ `[youtube http://www.youtube.com/watch?v=DTliSyBPBWI]`
+ 
+3. On the right hand side of the page editing screen in WordPress, you can change the visibility to Password protected and enter in a password.
+
+== Changelog ==
+= 1.0 = 
+* Initial Release
+
+= 1.0.1 =
+* Added compatiblity with the exclude pages plugin
+
+= 1.1.0 =
+* Updated the plugin to include the security enhancements added in WordPress 3.4
+
+= 1.1.1 =
+* The version labeled 1.1.0 hadn't included my final fixes and did not work actually work with 3.4. 1.1.1 simply includes the code that was intended to be 1.1.0.
+* Updated the styling of the changelog section of the readme file to allow it to parse correctly on WordPress.org
+
+= 1.1.2 =
+* Tweeked the Exclude Pages plugin compatibilty
+
+= 1.1.3 =
+* Added 3.6 compatiblity
+* Improved internationalization
+* Added support for plugins that replace the default hashing protocol
+
+= 1.1.4 =
+* Updated header to 3.8.1 compatibility
+* Improved documentation
+* NOTE: Released on the same day 1.1.3 was publicly checked in. This made 3.6+ compatibility universally available.
+
+= 1.1.5 =
+* Fixed an error on the Invalid Password response
---
+++ smartpwpages.php
@@ -0,0 +1,151 @@
+<?php
+/*
+Plugin Name: Smart Passworded Pages
+Plugin URI: http://thecodecave.com/plugins/smart-passworded-pages-plugin/
+Description: Allows a central login page for password protected child pages. Enter a password and you are taken to the newest child page with a matching password.
+Version: 1.1.5
+Author: Brian Layman
+Author URI: http://eHermitsInc.com/
+License: GPL2
+Requires: 2.5
+
+Copyright 2014  Brian Layman  (email : plugins@thecodecave.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as 
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * Define Constants
+ */
+define( 'SECONDS_TO_STORE_PW', 864000); // 864000 = 10 Days 
+
+/**
+ * Smart Passworded Pages Class
+ * @copyright Copyright (c), Brian Layman
+ * @author Brian Layman <plugins@TheCodeCave.com>
+ */
+ class smartPWPages {
+	/**
+	 * Smart Passworded Pages
+	 * Embeds a form for password submission into a post via a shortcode.
+	 */
+     function smartpwpages_shortcode( $atts ) {
+		global $post;
+		extract( shortcode_atts( array(
+			'label' => __( 'Enter', 'smartpwpages' ),
+			'ID' => 'smartPWLogin',
+		), $atts ) );
+		$result =  '<form ID="' . $ID . '" method="post" action="' . get_permalink() . '" >' . PHP_EOL;
+		if ( isset( $_GET['wrongpw'] ) ) $result .= '<p id="smartPWError">' . __( 'You\'ve entered an invalid password.</p>', 'smartpwpages' ) . PHP_EOL;
+		$result .= '	<input class="requiredField" type="password" name="smartPassword" id="smartPassword" value=""/>' . PHP_EOL;
+		$result .= '	<input type="hidden" name="smartParent" value="' .  $post->ID . '" />' . PHP_EOL;
+		$result .= '	<input type="hidden" name="smartPWPage_nonce" value="' . wp_create_nonce( smartPWPage ).'" />' . PHP_EOL;
+		$result .= '	<input type="submit" value="' . $label . '" />' . PHP_EOL;
+		$result .= '</form>' . PHP_EOL;
+		return $result;
+	}
+
+	/**
+	 * Password Redirect
+	 * Decodes the password, stores it in a cookie and redirects the visitor to that page.
+	 */
+	 function pw_redirect( $perma, $password ) {
+		global $wp_version, $wp_hasher;
+
+		// Version 3.6 introduces a new function
+		if ( function_exists( 'wp_unslash' ) ) {
+			$cookiePW = wp_unslash( $password );
+		} else {
+			$cookiePW = stripslashes( $password );
+		}
+
+		// Version 3.4 and higher has better security on the pw pages
+		if ( version_compare( $wp_version, '3.4', '>=' ) ) {
+			if ( empty( $wp_hasher ) ) {
+				// By default, use the portable hash from phpass
+				require_once( ABSPATH . 'wp-includes/class-phpass.php');
+				$wp_hasher = new PasswordHash( 8, true );
+			}
+
+			// Potentially using a custom hasher, hash the pw
+			$cookiePW = $wp_hasher->HashPassword( $cookiePW );
+		}		
+		
+		// Store password for the length in the constant
+		setcookie( 'wp-postpass_' . COOKIEHASH, $cookiePW, time() + SECONDS_TO_STORE_PW, COOKIEPATH );
+		wp_safe_redirect( $perma );
+		exit();
+	}
+	
+	/**
+	 * Process Form
+	 * Decodes the password submitted on a form, find a page that uses it and redirects the visitor to that page.
+	 */
+	function process_form() {
+		global $wp_hasher;
+		if ( isset( $_POST[ 'smartPassword' ] ) && isset( $_POST[ 'smartParent' ] ) && wp_verify_nonce( $_POST[ 'smartPWPage_nonce' ], smartPWPage ) ) {
+			$parentForm  = (int) $_POST[ 'smartParent' ] ;
+			$password = $_POST[ 'smartPassword' ];
+
+			if ( function_exists( 'wp_unslash' ) ) {
+				$postPassword = wp_unslash( $password );
+			} else {
+				$postPassword = stripslashes( $password );
+			}
+
+			$args = array(		
+				'sort_order' => 'DESC',
+				'sort_column' => 'post_date',
+				'hierarchical' => 1,
+				'child_of' => $parentForm,
+				'parent' => $parentForm,
+				'post_type' => 'page',
+				'post_status' => 'publish'
+			);
+			
+			if ( function_exists( 'pause_exclude_pages' ) ) pause_exclude_pages();
+
+			$myPages = get_pages( $args );
+
+			if ( function_exists( 'resume_exclude_pages' ) ) resume_exclude_pages();
+
+			// Version 3.4 and higher has better security on the pw pages
+			if ( version_compare( $wp_version, '3.4', '>=' ) ) {
+				if ( empty( $wp_hasher ) ) {
+					// By default, use the portable hash from phpass
+					require_once( ABSPATH . 'wp-includes/class-phpass.php' );
+					$wp_hasher = new PasswordHash( 8, true );
+				}
+			}
+
+			foreach( $myPages as $page ) {
+				if ( ( $page->post_password == $postPassword ) || ( !empty( $wp_hasher ) && $wp_hasher->CheckPassword( $post->post_password, $postPassword ) ) ) {
+					$permalink = get_permalink( $page->ID );
+					$this->pw_redirect( $permalink, $postPassword );
+				}
+			}
+
+			// Nothing more to do here. If we reached here, we've submitted a pw but no match was found. 
+			// Allow the page to continue loading, but hack $_GET to indicate the status
+			$_GET[ 'wrongpw' ] = TRUE;
+		}
+	}
+}
+
+/**
+ * Intialize Plugin
+ */
+$smartPWPages = new smartPWPages();
+add_action( 'init', array( $smartPWPages, 'process_form' ) );
+add_shortcode( 'smartpwpages', array( $smartPWPages, 'smartpwpages_shortcode' ) );
